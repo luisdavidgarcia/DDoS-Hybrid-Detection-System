8002fd1292d565b3cdcc8ca7f7b111905a061c8b
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-05-28 09:37:39 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -32,29 +32,29 @@ conda install -c conda-forge numpy pandas matplotlib -y
 # Install Ollama client if available
 pip install ollama
 
-# Create the Django project and app in the backend directory
-mkdir -p backend
-cd backend
-django-admin startproject $DJANGO_PROJECT_NAME
-cd $DJANGO_PROJECT_NAME
-python manage.py startapp $DJANGO_APP_NAME
+# # Create the Django project and app in the backend directory
+# mkdir -p backend
+# cd backend
+# django-admin startproject $DJANGO_PROJECT_NAME
+# cd $DJANGO_PROJECT_NAME
+# python manage.py startapp $DJANGO_APP_NAME
 
-# Update Django settings to include the new app and Django REST framework
-SETTINGS_FILE="$DJANGO_PROJECT_NAME/settings.py"
-sed -i "" "s/INSTALLED_APPS = \[/INSTALLED_APPS = \[\n    '$DJANGO_APP_NAME',\n    'rest_framework',/" $SETTINGS_FILE
+# # Update Django settings to include the new app and Django REST framework
+# SETTINGS_FILE="$DJANGO_PROJECT_NAME/settings.py"
+# sed -i "" "s/INSTALLED_APPS = \[/INSTALLED_APPS = \[\n    '$DJANGO_APP_NAME',\n    'rest_framework',/" $SETTINGS_FILE
 
-# Create initial Django migration
-python manage.py makemigrations
-python manage.py migrate
+# # Create initial Django migration
+# python manage.py makemigrations
+# python manage.py migrate
 
-# Go back to the root directory
-cd ../../
+# # Go back to the root directory
+# cd ../../
 
-# Create the React project in the frontend directory
-mkdir -p frontend
-cd frontend
-npx create-react-app $REACT_APP_NAME
-cd $REACT_APP_NAME
+# # Create the React project in the frontend directory
+# mkdir -p frontend
+# cd frontend
+# npx create-react-app $REACT_APP_NAME
+# cd $REACT_APP_NAME
 
 # Output success message
 echo "Conda environment '$ENV_NAME' has been set up with all dependencies."
