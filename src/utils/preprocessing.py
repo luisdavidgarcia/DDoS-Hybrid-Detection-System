70e5631703f884dc5ec375c9affbf30ed191af02
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-06-23 16:42:12 -0700

    Resolved issue

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -20,20 +20,26 @@ def split_train_test_data(df, target_column, test_size=0.2):
 
     return X_train, X_test, y_train, y_test
 
-def scale_X_train_test_data(X_train, X_test, numerical_features, categorical_features):
-    numerical_transformer = Pipeline(steps=[
-        ('scaler', StandardScaler())
-    ])
-    categorical_transformer = Pipeline(steps=[
-        ('onehot', OneHotEncoder(handle_unknown='ignore', sparse=False))
-    ])
+def scale_X_train_test_data(X_train, X_test, numerical_features=None, categorical_features=None):
+    transformers = []
 
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numerical_transformer, numerical_features),
-            ('cat', categorical_transformer, categorical_features)
-        ]
-    )
+    if numerical_features is None and categorical_features is None:
+        print("No features to scale or encode")
+        return
+    
+    if numerical_features is not None:
+        numerical_transformer = Pipeline(steps=[
+            ('scaler', StandardScaler())
+        ])
+        transformers.append(('num', numerical_transformer, numerical_features))
+        
+    if categorical_features is not None:
+        categorical_transformer = Pipeline(steps=[
+            ('onehot', OneHotEncoder(handle_unknown='ignore', sparse=False))
+        ])
+        transformers.append(('cat', categorical_transformer, categorical_features))
+        
+    preprocessor = ColumnTransformer(transformers=transformers)
 
     X_train_preprocessed = preprocessor.fit_transform(X_train)
     X_test_preprocessed = preprocessor.transform(X_test)
@@ -46,4 +52,4 @@ def encode_y_train_test_data(y_train, y_test):
     y_test_encoded = le.transform(y_test)
 
     return y_train_encoded, y_test_encoded, le
-    
+
