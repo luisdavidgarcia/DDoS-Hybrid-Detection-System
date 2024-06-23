f15510d4e2feff583cba2721a183e79837cad666
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-06-23 16:42:26 -0700

    Moved functions around

--- src/utils/data_visualization.py
+++ src/utils/data_visualization.py
@@ -1,6 +1,4 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 def data_preview(df):
     print("Data Frame Shape: ", df.shape)
@@ -40,31 +38,3 @@ def plot_label_distribution(df, target_column):
     plt.xlabel("Target Labels")
     plt.ylabel("Count")
     plt.show()
-
-def plot_top_features(top_features, X_train):
-    # top_features is a SelectKBest object
-    mask = top_features.get_support()
-    feature_scores = top_features.scores_
-
-    selected_features = X_train.columns[mask]
-    selected_scores = feature_scores[mask]
-
-    df_top_features =  pd.DataFrame({
-        'Feature': selected_features, 
-        'Score': selected_scores
-    })
-
-    df_top_features = df_top_features.sort_values(by='Score', ascending=False)
-
-    num_features = len(selected_features)
-    plt.figure(figsize=(10, 5))
-    plt.barh(df_top_features['Feature'], df_top_features['Score'])
-    plt.xlabel('Mutual Information Score')
-    plt.ylabel('Feature')
-    plt.title(f'Top {num_features} Features')
-    plt.gca().invert_yaxis()
-    plt.show()
-
-def plot_heatmap_correlation(df):
-    plt.figure(figsize=(16, 10))
-    sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
--- src/utils/feature_engineering.py
+++ src/utils/feature_engineering.py
@@ -2,6 +2,8 @@ from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import SelectKBest
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def select_top_features(X_train, y_train, k, verbose=False):
     mutual_info = mutual_info_classif(X_train, y_train)
@@ -17,3 +19,31 @@ def select_top_features(X_train, y_train, k, verbose=False):
     top_features.fit(X_train, y_train)
     
     return top_features
+
+def plot_top_features(top_features, X_train):
+    # top_features is a SelectKBest object
+    mask = top_features.get_support()
+    feature_scores = top_features.scores_
+
+    selected_features = X_train.columns[mask]
+    selected_scores = feature_scores[mask]
+
+    df_top_features =  pd.DataFrame({
+        'Feature': selected_features, 
+        'Score': selected_scores
+    })
+
+    df_top_features = df_top_features.sort_values(by='Score', ascending=False)
+
+    num_features = len(selected_features)
+    plt.figure(figsize=(10, 5))
+    plt.barh(df_top_features['Feature'], df_top_features['Score'])
+    plt.xlabel('Mutual Information Score')
+    plt.ylabel('Feature')
+    plt.title(f'Top {num_features} Features')
+    plt.gca().invert_yaxis()
+    plt.show()
+
+def plot_heatmap_correlation(df):
+    plt.figure(figsize=(16, 10))
+    sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
