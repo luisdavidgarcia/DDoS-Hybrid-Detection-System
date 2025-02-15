d08dffe29732c372cb67dc501b121b1a91927e6b
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-02-15 15:31:35 -0800

    updated filenames

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,9 +4,9 @@ rm logs/docker_stats_*.csv
 rm logs/eve.json
 rm logs/suricata.log
 
-rm models/ae_xgb/ae_xgb_binary_model_predictions.log
-rm models/cnn_lstm/cnn_lstm_binary_model_predictions.log
-rm models/dt/decision_tree_binary_model_predictions.log
-rm models/rf/random_forest_binary_model_predictions.log
-rm models/xgb/xgboost_binary_model_predictions.log
-rm models/lr/logistic_regression_binary_model_predictions.log
+rm models/ae_xgb/*predictions.log
+rm models/cnn_lstm/*predictions.log
+rm models/dt/*predictions.log
+rm models/rf/*predictions.log
+rm models/xgb/*predictions.log
+rm models/lr/*predictions.log
